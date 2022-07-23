4a7326396d30eb1870c5e2249f432944ffec1804
Anannya27 <104672818+Anannya27@users.noreply.github.com>
2022-07-23 18:20:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function take_snapshot()
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t61n6nIA7/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/bHnq5Chko/model.json',modelLoaded);
 
 function modelLoaded (){
     console.log("model_loaded");
